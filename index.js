8dba82ca266472b536dceb07bc1a1df0ce52e26d
Balalomba Barandao <barand14@gmail.com>
2024-05-01 12:40:42 -0400

    generated array of questions

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 // TODO: Include packages needed for this application
+const generatateMarkdown = require("./utils/generateMarkdown.js"); //importe generatateMarkdown function
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+    type: "input",
+    name: "title",
+    message: "Please enter the title of your application",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: " Enter a brief description of your application",
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "please enter any installationn instructions",
+            },
+
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.createPrompt(questions)
+}
 
 // Function call to initialize app
 init();
